94127f6498b99d3caad7d0151cd5a786802c4821
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-02-05 09:47:09 -0800

    remove root __init__ file

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'tfr2human',
   py_modules = ['tfr2human'],
   version = '0.0.0.1',
-  description = 'Python Multiprocessing helpers',
+  description = 'Convert TFRecords to images and JSON',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
   url = 'https://github.com/brookisme/tfr2human',
   download_url = 'https://github.com/brookisme/tfr2human/tarball/0.1',
-  keywords = ['TensorFlow','Tensor Flow Records','machine learning'],
+  keywords = ['TensorFlow','Tensor Flow Records','TFRecords,'machine learning'],
   include_package_data=True,
   data_files=[
     (
