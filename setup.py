230bb4c3008beecac29f94f1827bfc32ab9ee512
Brookie Guzder-Williams <bguzder-williams@wri.org>
2020-08-21 22:56:58 -0700

    [pypi 0.0.0.3] setup package info

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from distutils.core import setup
 setup(
   name = 'tfr2human',
-  py_modules = ['tfr2human'],
-  version = '0.0.0.2',
+  packages=[
+    'tfr2human',
+  ],
+  package_dir = {
+    'tfr2human': 'tfr2human'
+  }
+  version = '0.0.0.3',
   description = 'Convert TFRecords to images and JSON',
   author = 'Brookie Guzder-Williams',
   author_email = 'brook.williams@gmail.com',
   url = 'https://github.com/brookisme/tfr2human',
   download_url = 'https://github.com/brookisme/tfr2human/tarball/0.1',
   keywords = ['TensorFlow','Tensor Flow Records','TFRecords','machine learning'],
-  include_package_data=True,
+  include_package_data=False,
   data_files=[
     (
       'config',[]
